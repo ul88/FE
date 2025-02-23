5923bf348a37a27e9b093ecd37fb9d1939b1ff60
ul88 <hkimog13@naver.com>
2025-02-23 22:48:13 +0900

    add main page

--- postFE/src/App.tsx
+++ postFE/src/App.tsx
@@ -1,33 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+"use client"
+import { useState, useEffect } from 'react'
 import './App.css'
+import axios from "axios"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [page, setPage] = useState()
+
+  async function fetchPage(){
+    const {data} = await axios.get("http://localhost:8080/api/post", {
+      params : {
+        page : 1,
+        size : 10
+      }
+    })
+    console.log(data)
+    
+  }
+
+  useEffect(()=>{fetchPage(), []})
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav className="navbar">
+        <div className="logo">MyLogo</div>
+        <ul className="nav-links">
+            <li><a href="#">홈</a></li>
+            <li><a href="#">소개</a></li>
+            <li><a href="#">서비스</a></li>
+            <li>
+                <a href="#">
+                    로그인
+                </a>
+            </li>
+            <li ><a href="#">회원가입</a></li>
+            <li ><span>name</span></li>
+            <li >
+                <a href="javascript:void(0)">
+                    로그아웃
+                </a>
+            </li>
+            <li><a href="#">글쓰기</a></li>
+        </ul>
+        <div className="menu-toggle">☰</div>
+    </nav>
+    <main>
+    </main>
     </>
   )
 }
